2d13ac8383c07e3abd26640d9153f6e846962d44
PottierLoic <loic.pottier@live.fr>
2023-08-25 11:17:11 +0200

    random palette generation and saving

--- script/palette_selector.js
+++ script/palette_selector.js
@@ -6,8 +6,7 @@ db.collection('colors').get()
   .then(querySnapshot => {
     querySnapshot.forEach(doc => {
       let colors = doc.data().palette;
-      console.log(colors);
-      let gradient =`linear-gradient(to right, ${colors[0]}, ${colors[2]})`;
+      let gradient =`linear-gradient(to right, ${colors[0]}, ${colors[2]}, ${colors[5]})`;
 
       var circle = document.createElement("div");
       circle.classList.add("circle");
@@ -18,13 +17,12 @@ db.collection('colors').get()
         palette.prepend(circle);
       }
 
-
       circle.addEventListener("click", (event) => {
         var circles = document.querySelectorAll('.circle');
         circles.forEach(circle => {
           circle.removeAttribute('id', 'selected');
         });
-        circle.id = "selected"
+        circle.id = "selected";
         document.documentElement.style.setProperty('--background-color', colors[0]);
         document.documentElement.style.setProperty('--title-color', colors[1]);
         document.documentElement.style.setProperty('--paraph-color', colors[2]);
@@ -42,9 +40,38 @@ db.collection('colors').get()
         localStorage.setItem('palette_id', doc.id);
         palette.prepend(circle);
       })
-
       palette_amount++;
     });
+    var rand_circle = document.createElement("div");
+    rand_circle.classList.add("circle");
+    rand_circle.style.backgroundImage = `url('img/refresh.png')`;
+    rand_circle.style.backgroundSize = `cover`;
+    rand_circle.style.backgroundPosition = `center`;
+    palette.appendChild(rand_circle);
+    rand_circle.addEventListener("click", (event) =>{
+      var circles = document.querySelectorAll('.circle');
+        circles.forEach(circle => {
+          circle.removeAttribute('id', 'selected');
+        });
+        rand_circle.id = "selected";
+      random_colors = get_random_palette();
+      document.documentElement.style.setProperty('--background-color', random_colors[0]);
+      document.documentElement.style.setProperty('--title-color', random_colors[1]);
+      document.documentElement.style.setProperty('--paraph-color', random_colors[2]);
+      document.documentElement.style.setProperty('--symbol-color', random_colors[3]);
+      document.documentElement.style.setProperty('--highlighted-color', random_colors[4]);
+      document.documentElement.style.setProperty('--highlighted-color-2', random_colors[5]);
+      document.documentElement.style.setProperty('--inaccessible-color', random_colors[6]);
+      localStorage.setItem('background-color', random_colors[0]);
+      localStorage.setItem('title-color', random_colors[1]);
+      localStorage.setItem('paraph-color', random_colors[2]);
+      localStorage.setItem('symbol-color', random_colors[3]);
+      localStorage.setItem('highlighted-color', random_colors[4]);
+      localStorage.setItem('highlighted-color2', random_colors[5]);
+      localStorage.setItem('inaccessible-color', random_colors[6]);
+    });
+
+    palette_amount++;
   })
   .catch(error => {
     console.error('Error while retrieving documents data: ', error);
@@ -58,3 +85,21 @@ palette.addEventListener('mouseenter', (event) => {
 palette.addEventListener('mouseleave', (event) => {
   palette.style.height = `50px`;
 });
+
+function get_random_color() {
+  const char = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += char[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function get_random_palette() {
+  let palette = [];
+  for (let i = 0; i < 6; i++) {
+    palette[i] = get_random_color();
+  }
+  return palette;
+}
