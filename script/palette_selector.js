cd7c8bfb156159a636d1c9648272d6f65c7d55bd
PottierLoic <loic.pottier@live.fr>
2023-08-25 09:51:10 +0200

    palette selector

--- script/palette_selector.js
+++ script/palette_selector.js
@@ -1,19 +1,45 @@
-const btn = document.getElementById("testbutton");
-
-btn.addEventListener('click', (event) => {
-  document.documentElement.style.setProperty('--background-color', '#11151D');
-  document.documentElement.style.setProperty('--title-color', '#d85841');
-  document.documentElement.style.setProperty('--paraph-color', '#7f5056');
-  document.documentElement.style.setProperty('--symbol-color', '#D76c58');
-  document.documentElement.style.setProperty('--highlighted-color', '#D76c58');
-  document.documentElement.style.setProperty('--highlighted-color-2', '#D76c58');
-  document.documentElement.style.setProperty('--inaccessible-color', '#ff4d5c');
-});
-
 const palette = document.getElementById("palette-selector");
 
+let palette_amount = 0;
+
+db.collection('colors').get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      let colors = doc.data().palette;
+      console.log(colors);
+      let gradient =`linear-gradient(to right, ${colors[0]}, ${colors[2]})`;
+
+      var circle = document.createElement("div");
+      circle.classList.add("circle");
+      circle.style.background = gradient;
+
+      palette.appendChild(circle);
+
+      circle.addEventListener("click", (event) => {
+        var circles = document.querySelectorAll('.circle');
+        circles.forEach(circle => {
+          circle.removeAttribute('id', 'selected');
+        });
+        circle.id = "selected"
+        document.documentElement.style.setProperty('--background-color', colors[0]);
+        document.documentElement.style.setProperty('--title-color', colors[1]);
+        document.documentElement.style.setProperty('--paraph-color', colors[2]);
+        document.documentElement.style.setProperty('--symbol-color', colors[3]);
+        document.documentElement.style.setProperty('--highlighted-color', colors[4]);
+        document.documentElement.style.setProperty('--highlighted-color-2', colors[5]);
+        document.documentElement.style.setProperty('--inaccessible-color', colors[6]);
+      })
+
+      palette_amount++;
+    });
+  })
+  .catch(error => {
+    console.error('Error while retrieving documents data: ', error);
+  });
+
 palette.addEventListener('mouseenter', (event) => {
-  palette.style.height = `250px`;
+  let nb = 50 * palette_amount;
+  palette.style.height = `${nb}px`;
 });
 
 palette.addEventListener('mouseleave', (event) => {
